9e097f70d337ede1462062b0b417fd326f7090d3
Pedro Gabriel <98903288+pedroogaab@users.noreply.github.com>
2024-09-20 22:32:19 -0300

    Update and rename ex6.m to Ex6.m

--- Roteiro 02/Ex6.m
+++ Roteiro 02/Ex6.m
@@ -8,16 +8,16 @@ close all;
 y = u + delta; % calcula o sinal resultante
 
 figure; % abre uma nova figura
-subplot(3, 1, 1) % divide a figura em 3 linhas e 1 coluna e seleciona a primeira entrada para o gr·fico a seguir
+subplot(3, 1, 1) % divide a figura em 3 linhas e 1 coluna e seleciona a primeira entrada para o gr√°fico a seguir
 stem(n, u)
 ylabel('u[n]') % inclui uma legenda no eixo y
 
-subplot(3, 1, 2) % divide a figura em 3 linhas e 1 coluna e seleciona a primeira entrada para o gr·fico a seguir
+subplot(3, 1, 2) % divide a figura em 3 linhas e 1 coluna e seleciona a primeira entrada para o gr√°fico a seguir
 stem(n, delta)
 ylabel('delta[n-3]') % inclui uma legenda no eixo y
 
 subplot(3, 1, 3)
-stem(n, y) % plota o gr·fico do sinal
+stem(n, y) % plota o gr√°fico do sinal
 xlabel('Amostra (n)') % inclui uma legenda no eixo x
 ylabel('y[n]') % inclui uma legenda no eixo y
 
@@ -27,10 +27,10 @@ ylabel('y[n]') % inclui uma legenda no eixo y
 
 function [n, x] = degrau(n_min, n_max, n0)
     n = n_min:1:n_max; % define um vetor com valores entre n_min e n_max com incrementos de 1
-    x = zeros(size(n)); % define o sinal x, que È inicializado com valores zero e de mesmo tamanho do vetor n
-    idx = (n >= n0); % encontra os Ìndices do vetor n em s„o maiores que n0
-    x(idx) = 1; % para os Ìndices encontrados anteriormente, faz com que o vetor x seja 1
-    % retorna os ˙ltimos valores de n e x
+    x = zeros(size(n)); % define o sinal x, que √© inicializado com valores zero e de mesmo tamanho do vetor n
+    idx = (n >= n0); % encontra os √≠ndices do vetor n em s√£o maiores que n0
+    x(idx) = 1; % para os √≠ndices encontrados anteriormente, faz com que o vetor x seja 1
+    % retorna os √∫ltimos valores de n e x
 end
 
 function [n, delta] = pulso(n_min, n_max, j)
